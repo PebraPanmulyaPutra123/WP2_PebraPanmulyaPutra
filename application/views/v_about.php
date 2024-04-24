fd53e581be071562b7653b0cfbb66008435272a2
PebraPanmulyaPutra123 <pebrapanmulya67@gmail.com>
2024-04-24 07:44:43 +0700

    Pertemuan 3

---
+++ application/views/v_about.php
@@ -0,0 +1,22 @@
+<section>
+    <h1><?php echo $judul?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Pebra</li>
+        <li>Nama Belakang : Panmulya Putra</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Jalan Bima 3 Gandasari</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Tangerang</li>
+    </ul>
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Sepak-bola</li>
+        <li>Badminton</li>
+    </ul>
+</section>
